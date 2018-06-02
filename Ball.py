60ec3ac00cbd3ac7889dfc3b8b27308d2320d1b9
emwilkes <emiliewilkes@gmail.com>
2018-06-01 21:21:01 -0400

    Ball Class Added

---
+++ Ball.py
@@ -0,0 +1,14 @@
+# File for the Ball Class
+
+class Ball():
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.radius = 1
+        self.density = 1
+        numBalls += 1
+    
+    numBalls = 0
+    
