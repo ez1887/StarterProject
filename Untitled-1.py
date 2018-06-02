76605f1e64ca109dc1763161d9c258b02a759a31
Eric Zhang <Eric@erics-mbp.home>
2018-06-01 20:12:42 -0400

    Dog Simulator

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,17 @@
-print("HI")
-print("HELLO WORLD")
-x = 3 + 6
-print(x)
-print("This is a new edit")
-print(5**4)
+class Dog():
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+    def setName(self, name):
+        self.name = name
+    def getName(self):
+        print(self.name)
+    def bark(self):
+        print("WOOF!")
+
+rex = Dog("Rex", "Brown")
+rex.getName()
+rex.setName("Rexy Boy")
+rex.getName()
+rex.bark()
+rex.bark()
