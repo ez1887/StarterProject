d27b5d6b6b71cfd2184628dab7811d408a8c8e46
ez1887 <ez1887@gmail.com>
2018-06-01 22:20:40 -0400

    Finished Ball Collision Update

--- Check_Ball_Collision.py
+++ Check_Ball_Collision.py
@@ -14,3 +14,15 @@ def check_ball_collision(ball_one, ball_two):
 def update_ball_collision(ball_one, ball_two):
     mass_one = ball_one.density * (4/3) * np.pi * ball_one.radius**3
     mass_two = ball_two.density * (4/3) * np.pi * ball_two.radius**3
+    v1fx = (((mass_one - mass_two)/(mass_one + mass_two)) * ball_one.vx + 
+        (2*mass_two*ball_two.vx)/(mass_one + mass_two))
+    v1fy = (((mass_one - mass_two)/(mass_one + mass_two)) * ball_one.vy + 
+        (2*mass_two*ball_two.vy)/(mass_one + mass_two))
+    v2fx = (((2 * mass_one)/(mass_one + mass_two)) * ball_one.vx + 
+        (mass_two - mass_one)/(mass_one + mass_two) * ball_two.vx)
+    v2fy = (((2 * mass_one)/(mass_one + mass_two)) * ball_one.vy + 
+        (mass_two - mass_one)/(mass_one + mass_two) * ball_two.vy)
+    ball_one.vx = v1fx
+    ball_one.vy = v1fy
+    ball_two.vx = v2fx
+    ball_two.vy = v2fy
